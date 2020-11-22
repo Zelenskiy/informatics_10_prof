df4cdeb70e078a9b2c1f7b8aca3b699ccfe51fa5
Zelenskiyas <zelensk1973@gmail.com>
2020-11-22 13:57:15 +0200

    feat: add create multiobject

--- Урок 41, 42. Перевизначення та розширення можливостей методів/Перевизначення методів.py
+++ Урок 41, 42. Перевизначення та розширення можливостей методів/Перевизначення методів.py
@@ -15,8 +15,32 @@ class Refrigerator(Machinery):
     def wholesalePrice(self, discount):
         return self.price * (1-discount / 100)        
 
+n = int(input("Уведіть кількість одиниць обладнання "))
+list = []
+for i in range(1,n+1):
+    print("----------------------------------")
+    print("Уведемо дані елемента №",i)
+    list.append(Refrigerator(input("Назва обладнання "), 
+    int(input("Ціна ")), input("Фірма виробник "), 
+    int(input("Потужність ")), int(input("Кількість камер "))))
 
-m = Refrigerator("Холодильник", 11000, "Samsung", 800, 2)
-m.about()
-print("Вартість зі знижкою", m.wholesalePrice(20))
+print("----------------------------------")
+print("Список обладнання")
+print("----------------------------------")
+for m in list:
+    print("Назва обладнання", m.name)
+    print("Ціна обладнання", m.price)
+    print("Потужність", m.power)
+    print("Кількість камер", m.numberOfCameras)
+    print("----------------------------------")
+
+
+
+
+
+
+#m = Refrigerator("Холодильник", 11000, "Samsung")
+# m = Refrigerator("Холодильник", 11000, "Samsung", 800, 2)
+# m.about()
+# print("Вартість зі знижкою", m.wholesalePrice(20))
 
